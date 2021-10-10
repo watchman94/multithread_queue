c7badcc9fb7980ae9f66345260c4f28c7452dd25
XUE Kaiwen <xuekaiwen94@gmail.com>
2021-10-10 19:13:20 +0800

    Clion git test - add multithread queue
    (cherry picked from commit a76345b6dab089885b76c5dda5b1653cff3b18ce)

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, Sunday!" << std::endl;
+    std::cout << "Hello, Monday!" << std::endl;
     ThreadsafeQueue<int> q;
     q.push(0);
     return 0;
